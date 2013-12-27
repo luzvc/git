364c450a2fd5f944e4d19e9f8ab03c187cca1894
Bruno Azisaka Maciel <bruno@azisaka.com.br>
2013-12-27 17:55:33 -0200

    Added global gitconfig

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,17 @@
 include_recipe "apt"
 
 package "git-core"
+
+node['git']['users'].each do |user_hash_key, user_hash|
+  template "/home/vagrant/.gitconfig" do
+    source "gitconfig.erb"
+    owner user_hash_key
+    mode "644"
+    action :create_if_missing
+    variables({
+      :name => user_hash[:name],
+      :email => user_hash[:email],
+      :github => user_hash[:github]
+    })
+  end
+end
