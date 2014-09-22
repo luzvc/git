164cfb4bcae42fc26553df41529352854d36dcbb
Thiago Dias <tdtadeu@gmail.com>
2014-09-21 22:17:26 -0300

    Removing development recipe. This cookbook will only install development environment

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,26 @@
-#
-# Cookbook Name:: git
-# Recipe:: default
-#
-# Copyright 2013, Bubble
-#
 include_recipe "apt"
 
-package "git-core"
+def setup(users)
+  install_git_core
+
+  users.each do |user|
+    download_gitconfig(user)
+  end
+end
+
+def install_git_core
+  package "git-core"
+end
+
+def download_gitconfig(user)
+  remote_file "Create .gitconfig" do
+    path "/home/#{user}/.gitconfig"
+    user user
+    source "#{node['git']['gist']}"
+    not_if { File.exists?("/home/#{user}/.gitconfig") }
+  end
+end
+
+users = node['git']['users']
+
+setup(users)
--- recipes/development.rb
+++
@@ -1,8 +0,0 @@
-node['git']['users'].each do |user|
-  remote_file "Create .gitconfig" do
-    path "/home/#{user}/.gitconfig"
-    user user
-    source "#{node['git']['gist']}"
-    not_if { File.exists?("/home/#{user}/.gitconfig") }
-  end
-end
