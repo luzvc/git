382be71032e9fe292f507a8f7e0c919c61df9546
Thiago Dias <tdtadeu@gmail.com>
2014-09-21 21:38:03 -0300

    Fetching gitconfig from gist

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'git'
-maintainer       'Bubble'
-maintainer_email 'contact@bubble.com.br'
+maintainer       'tdtadeu'
+maintainer_email 'tdtadeu@gmail.com'
 license          'MIT'
 description      'Installs/Configures git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends          'apt'
--- recipes/development.rb
+++ recipes/development.rb
@@ -1,15 +1,8 @@
 node['git']['users'].each do |user|
-  template "/home/#{user}/.gitconfig" do
-    source "gitconfig.erb"
-    owner user
-    mode "644"
-    action :create_if_missing
-  end
-
   remote_file "Create .gitconfig" do
     path "/home/#{user}/.gitconfig"
     user user
-    source "#{node['vim']['gist']}"
+    source "#{node['git']['gist']}"
     not_if { File.exists?("/home/#{user}/.gitconfig") }
   end
 end
